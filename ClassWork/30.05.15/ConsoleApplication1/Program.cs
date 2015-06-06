a417851742bfc8c30c8d5c9f5ac6cf6087131c79
shartep <shartep@mail.ru>
2015-06-06 19:27:07 +0300

    06-06-15

--- ClassWork/30.05.15/ConsoleApplication1/Program.cs
+++ ClassWork/30.05.15/ConsoleApplication1/Program.cs
@@ -5,31 +5,112 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace ConsoleApplication1
 {
     class Program
     {
+        static SqlConnection connection = new SqlConnection(  @"Data Source=(localdb)\Projects;
+                                                                Initial Catalog=Database1;
+                                                                Integrated Security=True;
+                                                                Connect Timeout=30;Encrypt=False;
+                                                                TrustServerCertificate=False");
         static void Main(string[] args)
         {
-            SqlConnection connection = new SqlConnection( @"Data Source=(localdb)\Projects;
-                                                            Initial Catalog=Database1;
-                                                            Integrated Security=True;
-                                                            Connect Timeout=30;Encrypt=False;
-                                                            TrustServerCertificate=False");
             connection.Open();
-            SqlCommand command = new SqlCommand("SELECT * FROM Teams", connection);
-            SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
+
+            SqlCommand command = new SqlCommand
             {
-                for (int i = 0; i < 6; i++)
+                Connection = connection,
+            };
+
+            //using (var transaction = connection.BeginTransaction("Data init"))
+            //{
+            //    command.Transaction = transaction;
+            //    var mergeCommandText = File.ReadAllText(@"..\..\..\Database1\Reference Data\Teams.sql").Replace("GO", "");
+            //    command.CommandText = mergeCommandText;
+            //    Console.WriteLine(command.ExecuteNonQuery() + " rows marged");
+            //    command.CommandText = @"DELETE FROM [dbo].[Teams] WHERE [Name] = N'Team E';";
+            //    var rowsDeleted = command.ExecuteNonQuery();
+            //    Console.WriteLine(rowsDeleted + " rows with Team E, deleted");
+            //    if (rowsDeleted > 0)
+            //        transaction.Commit();
+            //    else
+            //        transaction.Rollback();
+            //    command.Transaction = null;
+            //}
+            command.CommandText = "[dbo].[InitData]";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("@delete", SqlDbType.Bit).Value = true;
+            SqlParameter parametr;
+            parametr = command.Parameters.Add(new SqlParameter("@deleted", SqlDbType.Int)
                 {
-                    Console.Write(reader[i]);
-                    Console.Write(' ');
+                    Direction = ParameterDirection.Output,
+                });
+            command.ExecuteNonQuery();
+            Console.WriteLine(parametr.Value + " rows with Team E deleted");
+            command.CommandType = CommandType.Text;
+
+            ReadData();
+
+            command.CommandText =
+                @"INSERT INTO [dbo].[Teams] ([Name], [Wins], [Losses], [Draws], [PictureUrl])
+                  VALUES (@teamName, @wins, @losses, @draws, @url);";
+            parametr = command.Parameters.AddWithValue("@teamName", "Team E");
+            parametr = command.Parameters.AddWithValue("@wins", 7);
+            parametr = command.Parameters.AddWithValue("@losses", 4);
+            parametr = command.Parameters.AddWithValue("@draws", 6);
+            parametr = command.Parameters.AddWithValue("@url", "Another URL");
+/*            command.CommandText =
+                @"INSERT INTO [dbo].[Teams] ([Id], [Name], [Wins], [Losses], [Draws], [PictureUrl])
+                  VALUES (N'7E452A9A-A2D3-48DB-BC62-E9954D04F210', N'Team E', 7, 4, 6, N'Another URL')"; */
+            Console.WriteLine(command.ExecuteNonQuery().ToString() + " rows inserted");
+            command.Parameters.Clear();
+
+            command.CommandText =
+                @"UPDATE [dbo].[Teams]
+                  SET [PictureUrl] = @url
+                  WHERE [Id] = @id;";
+            command.Parameters.AddWithValue("@url", "45463485L");
+            command.Parameters.AddWithValue("@id", "da36bc84-b984-4554-aaaf-71a2a5c7058f");
+/*            command.CommandText =
+                @"UPDATE [dbo].[Teams]
+                  SET [PictureUrl] = N'Updated URL'
+                  WHERE [Id] = N'da36bc84-b984-4554-aaaf-71a2a5c7058f';";  */
+            Console.WriteLine(command.ExecuteNonQuery().ToString() + " rows updated");
+            command.Parameters.Clear();
+
+            command.CommandText =
+                @"DELETE FROM [dbo].[Teams]
+                  WHERE [Id] = @id";
+            command.Parameters.AddWithValue("@id", "31114623-5dd7-467f-926d-b4d56f07077f");
+/*            command.CommandText =
+                @"DELETE FROM [dbo].[Teams]
+                  WHERE [Id] = N'31114623-5dd7-467f-926d-b4d56f07077f';";*/
+            Console.WriteLine(command.ExecuteNonQuery().ToString() + " rows deleted");
+
+            ReadData();
+            Console.ReadKey();
+        }
+
+        private static void ReadData()
+        {
+            SqlCommand selectCommand = new SqlCommand("SELECT * FROM Teams", connection);
+            using (SqlDataReader reader = selectCommand.ExecuteReader())
+            {
+                Console.WriteLine();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < 6; i++)
+                    {
+                        Console.Write(reader[i]);
+                        Console.Write(' ');
+                    }
+                    Console.WriteLine();
                 }
                 Console.WriteLine();
             }
-            Console.ReadKey();
         }
     }
 }
---
+++ ClassWork/30.05.15/Database1/Procedures/InitData.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[InitData]
+	@delete bit = 0,
+	@deleted int out
+AS
+BEGIN
+	MERGE INTO [dbo].[Teams] AS Target
+	USING ( VALUES
+		(N'4b13ccec-c2df-4858-94e1-4488c3dce7af', N'Team A', 2, 3, 5, N'Some URL'),
+		(N'67da61cb-7586-48bf-8da5-d393f6f20727', N'Team B', 7, 1, 2, N'Some URL'),
+		(N'da36bc84-b984-4554-aaaf-71a2a5c7058f', N'Team C', 6, 3, 3, N'Some URL'),
+		(N'31114623-5dd7-467f-926d-b4d56f07077f', N'Team D', 1, 5, 4, N'Some URL')	
+	) AS Source ([Id], [Name], [Wins], [Losses], [Draws], [PictureUrl])
+	ON Target.[Id] = Source.[Id]
+	WHEN NOT MATCHED BY TARGET THEN
+	-- Insert new rows
+	INSERT ([Id], [Name], [Wins], [Losses], [Draws], [PictureUrl])
+	VALUES ([Id], [Name], [Wins], [Losses], [Draws], [PictureUrl]);
+	
+	IF @delete = 1
+	BEGIN
+		DELETE FROM [dbo].[Teams] WHERE [Name] = N'Team E';
+		SET @deleted = @@ROWCOUNT;
+	END
+END
+RETURN @@ROWCOUNT
